9ea30f106646d22614d954eeeadb3b5128d92621
Jacob Marsay <jacobmarsay@calls9.com>
2022-12-21 16:22:17 +0000

    Added a nested route

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,11 +1,14 @@
-import { useParams } from "react-router-dom";
-
+import { useParams, Routes, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 const QuoteDetails = () => {
   const params = useParams();
   return (
     <div>
       <h1>Quote Details</h1>
       <p>{params.quoteId}</p>
+      <Routes>
+        <Route path={`/comments`} element={<Comments />}></Route>
+      </Routes>
     </div>
   );
 };
