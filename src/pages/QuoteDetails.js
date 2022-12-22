208f8c2531b32b00adb7b64385f204d4e3acd06c
Jacob Marsay <jacobmarsay@calls9.com>
2022-12-22 17:54:21 +0000

    Displaying quote details

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,11 +1,13 @@
 import { useParams, Routes, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import { DUMMY_DATA } from "../data/DummyData";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 const QuoteDetails = () => {
   const params = useParams();
+  const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
   return (
     <div>
-      <h1>Quote Details</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Routes>
         <Route path={`/comments`} element={<Comments />}></Route>
       </Routes>
